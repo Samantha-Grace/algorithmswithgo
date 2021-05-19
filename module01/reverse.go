f921919488ccb284634513bcb43e1ae22411deae
Samantha-Grace <sgrace732@gmail.com>
2021-05-19 15:51:11 -0600

    reverse a string

--- module01/reverse.go
+++ module01/reverse.go
@@ -6,6 +6,33 @@ package module01
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
-func Reverse(word string) string {
-	return ""
+func Reverse(word string) (result string) {
+	for _, v := range word {
+		result = string(v) + result
+	}
+	return
 }
+
+// func Reverse(word string) string {
+// 	var res string
+// 	for _, r := range word {
+// 		res = string(r) + res
+// 	}
+// 	return res
+// }
+
+// func Reverse(word string) string {
+// 	var res string
+// 	for i := len(word) - 1; i >= 0; i-- {
+// 		res = res + string(word[i])
+// 	}
+// 	return res
+// }
+
+// func Reverse(word string) string {
+// 	var res string
+// 	for i := 0; i < len(word); i++ {
+// 		res = string(word[i]) + res
+// 	}
+// 	return res
+// }
